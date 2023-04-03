c60fea8c71ffdb7871484c61e233d3e74f57f587
ponbac <root@ponbac.xyz>
2023-04-03 20:18:39 +0200

    some prog

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,6 @@
-use axum::{extract::Path, http::StatusCode, routing::get, Router};
-use std::net::SocketAddr;
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() {
-    // A handler is an async function which returns something that implements
-    // `axum::response::IntoResponse`.
-    let app = Router::new()
-        .route("/", get(handler))
-        .route("/health_check", get(health_check))
-        .route("/hello/:name", get(hello));
-
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-}
-
-async fn handler() -> &'static str {
-    "Hello, world!"
-}
-
-async fn health_check() -> StatusCode {
-    StatusCode::OK
-}
-
-async fn hello(Path(name): Path<String>) -> String {
-    format!("Hello, {}!", name)
+    run().await
 }
