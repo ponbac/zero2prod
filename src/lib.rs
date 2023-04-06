c306ff4ce8376d8abdcc00a1da9ab557889727c5
ponbac <root@ponbac.xyz>
2023-04-06 22:52:32 +0200

    hmm, status codes :/

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
-use axum::{extract::Path, http::StatusCode, routing::get, Router};
+use axum::{
+    extract::Path,
+    http::StatusCode,
+    routing::{get, post},
+    Form, Router,
+};
 
 pub fn app() -> Router {
     Router::new()
         .route("/", get(handler))
         .route("/health_check", get(health_check))
         .route("/hello/:name", get(hello))
+        .route("/subscriptions", post(subscribe))
 }
 
 async fn handler() -> &'static str {
@@ -17,4 +23,32 @@ async fn health_check() -> StatusCode {
 
 async fn hello(Path(name): Path<String>) -> String {
     format!("Hello, {}!", name)
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String,
+}
+
+async fn subscribe(Form(form_data): Form<FormData>) -> StatusCode {
+    match (form_data.name.is_empty(), form_data.email.is_empty()) {
+        (true, true) => return StatusCode::BAD_REQUEST,
+        (true, false) => {
+            println!("Missing name");
+            return StatusCode::BAD_REQUEST;
+        }
+        (false, true) => {
+            println!("Missing email");
+            return StatusCode::BAD_REQUEST;
+        }
+        (false, false) => (),
+    }
+
+    print!(
+        "Welcome, {}! We saved your email: {}",
+        form_data.name, form_data.email
+    );
+
+    StatusCode::OK
 }
