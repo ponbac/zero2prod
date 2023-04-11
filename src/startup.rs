8a971957b43a0d22deb662a39959309f56357410
ponbac <root@ponbac.xyz>
2023-04-11 20:56:16 +0200

    added logging

--- src/startup.rs
+++ src/startup.rs
@@ -5,6 +5,7 @@ use axum::{
     Router,
 };
 use sqlx::PgPool;
+use tower_http::trace::TraceLayer;
 
 use crate::routes::{health_check, subscribe};
 
@@ -14,6 +15,9 @@ pub struct AppState {
 }
 
 pub fn app_router(db_pool: PgPool) -> Router {
+    // initialize the logger
+    tracing_subscriber::fmt::init();
+
     Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .route("/health_check", get(health_check))
@@ -21,4 +25,6 @@ pub fn app_router(db_pool: PgPool) -> Router {
         .with_state(AppState {
             db_pool: Arc::new(db_pool),
         })
+        // use with RUST_LOG=tower_http=debug to see the logs
+        .layer(TraceLayer::new_for_http())
 }
