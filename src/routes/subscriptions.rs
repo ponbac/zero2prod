896f32d4dc3c429b7908deeecc7ae3b2f99b0040
ponbac <root@ponbac.xyz>
2023-04-22 13:37:30 +0200

    better

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use axum::{extract::State, Form};
 use chrono::Utc;
 use reqwest::StatusCode;
+use tracing::Instrument;
 use uuid::Uuid;
 
 use crate::startup::AppState;
@@ -16,11 +17,12 @@ pub async fn subscribe(
     Form(form_data): Form<FormData>,
 ) -> StatusCode {
     tracing::info!(
-        "Adding new subscriber, {} <{}>",
+        "Adding new subscriber: {} <{}>",
         form_data.name,
         form_data.email
     );
 
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -32,12 +34,10 @@ pub async fn subscribe(
         Utc::now()
     )
     .execute(app_state.db_pool.as_ref())
+    .instrument(query_span)
     .await
     {
-        Ok(_) => {
-            tracing::info!("Successfully added new subscriber");
-            StatusCode::OK
-        }
+        Ok(_) => StatusCode::OK,
         Err(e) => {
             tracing::error!("Failed to execute query. Error: {:?}", e);
             StatusCode::INTERNAL_SERVER_ERROR
