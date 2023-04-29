4f928e1dd4eb96ff0ddcef5e9b021f0bdf986e00
ponbac <root@ponbac.xyz>
2023-04-29 12:46:54 +0200

    more conf

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use zero2prod::{
 
 #[tokio::main]
 async fn main() {
+    // Init telemetry
     let subscriber = telemetry::get_subscriber(
         "zero2prod".into(),
         "info,tower_http=debug,axum=debug,sqlx=debug",
@@ -17,16 +18,29 @@ async fn main() {
     );
     telemetry::init_subscriber(subscriber);
 
+    // Read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
+
+    // Setup database
+    let connection_string = std::env::var("DATABASE_URL").unwrap_or_else(|_| {
+        configuration
+            .database
+            .connection_string()
+            .expose_secret()
+            .to_string()
+    });
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(3))
-        .connect_lazy(configuration.database.connection_string().expose_secret())
+        .connect_lazy(&connection_string)
         .expect("Failed to create Postgres connection pool.");
+
+    // Create host address
     let port = std::env::var("PORT").unwrap_or_else(|_| configuration.application.port.to_string());
     let socket_addr = format!("{}:{}", configuration.application.host, port)
         .parse::<SocketAddr>()
         .expect("Failed to parse address.");
 
+    // Start server
     tracing::info!(
         "Starting application on {}:{}",
         configuration.application.host,
