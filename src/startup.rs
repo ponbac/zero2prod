e79bb9fadad67763757ed274dc25c84071ee62a0
ponbac <root@ponbac.xyz>
2023-04-30 10:14:48 +0200

    weird!

--- src/startup.rs
+++ src/startup.rs
@@ -21,7 +21,7 @@ pub struct RequestSpan;
 
 impl<B> tower_http::trace::MakeSpan<B> for RequestSpan {
     fn make_span(&mut self, request: &http::Request<B>) -> tracing::Span {
-        tracing::error_span!(
+        tracing::debug_span!(
             "rq",
             id = %Uuid::new_v4(),
             method = %request.method(),
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -72,6 +72,38 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 }
 
 #[tokio::test]
+async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+    // Arrange
+    let test_app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula%20K&email=", "empty email"),
+        ("name=Ursula%20K&email=def-not-an-email", "invalid email"),
+    ];
+
+    for (body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("http://{}/subscriptions", test_app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Assert
+        assert_eq!(
+            200,
+            response.status().as_u16(),
+            // Additional customised error message on test failure
+            "The API did not return a 200 OK when the payload was {}.",
+            error_message
+        );
+    }
+}
+
+#[tokio::test]
 async fn subscribe_returns_a_422_when_data_is_missing() {
     // Arrange
     let test_app = spawn_app().await;
@@ -140,6 +172,7 @@ pub async fn configure_database(db_settings: &DatabaseSettings) -> PgPool {
         .execute(format!(r#"CREATE DATABASE "{}";"#, db_settings.database_name).as_str())
         .await
         .expect("Failed to create database.");
+
     // Migrate database
     let connection_pool = PgPool::connect_with(db_settings.with_db())
         .await
